5a31d1502c84e0fd4d2bcf0fb1656c353182bd61
qjvkdkel12 <52388914+qjvkdkel12@users.noreply.github.com>
2019-08-22 09:58:22 +0900

    Add files via upload

---
+++ 내가 공부한 것/easy_r/R_File/Fun.R
@@ -0,0 +1,22 @@
+# 변수 만들기
+x <- c(1,2,3)
+x
+
+mean(x)
+
+max(x)
+min(x)
+
+str5 <- c("Life", "is", "short", "you", "need", "python")
+str5
+
+paste(str5, collapse = ",")
+str5
+
+str6 = "Life is too short you need python"
+str6
+
+paste(str6, collapse = "ab")
+
+x_mean <- mean(x) # x변수의 평균값을 x_mean변수에 대입
+x_mean
---
+++ 내가 공부한 것/easy_r/R_File/String.R
@@ -0,0 +1,17 @@
+str1 <- "a"
+str1
+
+str2 <- "abc"
+str2
+
+str3 <- "Hello R World"
+str3
+
+str4 <- c("a,", "b", "c")
+str4
+
+str5 <- c("Hello", "World", "is", "good!")
+str5
+
+#str1+2 *문자로 된 변수는 연산이 불가능.
+ㄴ
